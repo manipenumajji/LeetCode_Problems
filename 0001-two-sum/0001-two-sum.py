1d5293cd5c8b89506dab74ab248679698e09d0b4
manipenumajji <manipenumajji0@gmail.com>
2026-07-10 19:11:31 +0530

    Time: 0 ms (100%), Space: 20.5 MB (17.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,13 @@
 class Solution:
-    def twoSum(self, nums, target):
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if target == nums[i] + nums[j]:
-                    return [i, j]
-
-x = Solution()
-print(x.twoSum([2, 7, 11, 15], 9))
-print(x.twoSum([3, 2, 4], 6))        
-print(x.twoSum([3, 3], 6))            
-               
-       
-        
+    def twoSum(self, nums,target): 
+        seen={}
+        for index,num in enumerate(nums):
+            needed=target-num
+            if needed in seen:
+                return [seen[needed],index]
+            seen[num]=index    
+nums=[2,7,11,15]
+target=9
+s=Solution()
+res=s.twoSum(nums,target)
+print(res)        

1b3d2979a7ced327ed4074e050f6f497f8ccefb5
manipenumajji <manipenumajji0@gmail.com>
2026-07-10 20:31:41 +0530

    Time: 24 ms (17.41%), Space: 37.6 MB (5.25%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,18 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen={}
+        for i in range(len(nums)):
+            if nums[i] not in seen:
+                seen[nums[i]]=1
+            else:
+                seen[nums[i]]+=1
+                return True 
+        return False        
+
+                
+                     
+nums=[1,2,3,1]
+s=Solution()
+res=s.containsDuplicate(nums)
+print(res)        
+        

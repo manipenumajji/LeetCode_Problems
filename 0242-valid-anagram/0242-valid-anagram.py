43157db1abf1594b7f0e8914c7f0f15909a08680
manipenumajji <manipenumajji0@gmail.com>
2026-07-10 23:39:45 +0530

    Time: 12 ms (55.64%), Space: 19.6 MB (29.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,9 +1,23 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if sorted(s)==sorted(t):
+        seen={}
+        freq={}
+        for i in s:
+            if i not in seen:
+                seen[i]=1
+            else:
+                seen[i]+=1
+        for j in t:
+            if j not in freq:
+                freq[j]=1
+            else:
+                freq[j]+=1
+        if seen==freq:
             return True
         else:
-            return False            
+            return False                
+
+                   
 s="anagram"
 t="nagaram"
 S=Solution()
